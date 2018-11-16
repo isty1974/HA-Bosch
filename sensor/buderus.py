83e23e68020b4e381d6f04f1362490366333f674
Istvan <isty1974@gmail.com>
2018-11-16 17:37:50 +0100

    First replacement - test

--- sensor/buderus.py
+++ sensor/buderus.py
@@ -1,10 +1,10 @@
 """
-Platform to read a Buderus KM200 unit.
+Platform to read a Bosch Pro Control Gateway unit.
 """
 import logging
 
-from custom_components.buderus import (
-    DOMAIN, BuderusBridge)
+from custom_components.bosch import (
+    DOMAIN, BoschBridge)
 from homeassistant.const import (
     CONF_RESOURCES, TEMP_CELSIUS)
 from homeassistant.helpers.entity import Entity
@@ -17,6 +17,103 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
 
     global SENSOR_TYPES
     SENSOR_TYPES = {
+        'fuel_caloric_value': [
+            'Fuel Caloric Value',
+            none,
+            'mdi:chart-histogram',
+            '/system/heatSources/hs1/fuel/caloricValue'
+        ],
+        'energy_monitoring_consumption': [
+            'Energy Monitoring Consumption',
+            none,
+            'mdi:chart-line-variant',
+            '/heatSources/energyMonitoring/consumption'
+        ],
+        'em_max_tank_level': [
+            'Energy Monitoring Max Tank Level',
+            none,
+            'mdi:chart-line-variant',
+            '/heatSources/energyMonitoring/maxTankLevel'
+        ],
+        'em_min_tank_level': [
+            'Energy Monitoring Min Tank Level',
+            none,
+            'mdi:chart-line-variant',
+            '/heatSources/energyMonitoring/minTankLevel'
+        ],
+        'em_tank_level': [
+            'Energy Monitoring Tank Level',
+            none,
+            'mdi:chart-line-variant',
+            '/heatSources/energyMonitoring/tankLevel'
+        ],
+        'fuel_caloric_value': [
+            'Energy Monitoring Tank Level',
+            none,
+            'mdi:fuel',
+            '/heatSources/hs1/fuel/caloricValue'
+        ],
+        'energy_reservoir': [
+            'Energy Reservoir',
+            none,
+            'mdi:chart-multiline',
+            '/heatSources/hs1/energyReservoir'
+        ],
+        'energy_reservoir_alert': [
+            'Energy Reservoir Alert',
+            none,
+            'mdi:chart-multiline',
+            '/heatSources/hs1/reservoirAlert'
+        ],
+        'system_energy_reservoir': [
+            'System Energy Reservoir',
+            none,
+            'mdi:chart-multiline',
+            '/system/heatSources/hs1/energyReservoir'
+        ],
+        'system_energy_reservoir_alert': [
+            'System Energy Reservoir Alert',
+            none,
+            'mdi:chart-multiline',
+            '/system/heatSources/hs1/reservoirAlert'
+        ],
+        'gw_datetime': [
+            'Gateway Date Time',
+            none,
+            'mdi:calendar-range',
+            '/gateway/DateTime'
+        ],
+        'gw_uuid': [
+            'Gateway UUID',
+            none,
+            'mdi:barcode',
+            '/gateway/uuid'
+        ],
+        'gw_version_firmware': [
+            'Gateway Firmware Version',
+            none,
+            'mdi:select-inverse',
+            '/gateway/versionFirmware'
+        ],
+        'gw_versionhardware': [
+            'Gateway Hardware Version',
+            none,
+            'mdi:engine',
+            '/gateway/versionHardware'
+        ],
+        'system_bus': [
+            'System Bus',
+            none,
+            'mdi:bus-side',
+            '/system/bus'
+        ],
+        'system_health_status': [
+            'System Health Status',
+            none,
+            'mdi:home-variant',
+            '/system/healthStatus'
+        ],
+        
         'return_temperature': [
             'Return Temperature',
             TEMP_CELSIUS,
@@ -34,6 +131,12 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
             TEMP_CELSIUS,
             'mdi:thermometer',
             '/system/sensors/temperatures/outdoor_t1'
+        ],
+        'min_outdoor_temp': [
+            'Minimum Outdoor Temperature',
+            TEMP_CELSIUS,
+            'mdi:thermometer',
+            '/system/minOutdoorTemp'
         ],
         'supply_temperature': [
             'Supply Temperature',
@@ -98,14 +201,14 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
         'boiler_flame': [
             'Boiler Flame',
             None,
-            None,
+            'mdi:fire',
             '/heatSources/flameStatus'
         ],
          'boiler_starts': [
             'Boiler Starts',
             None,
-            None,
-            '/heatSources/hs1/numberOfStarts'
+            'mdi:chervon_double_up',
+            '/heatSources/numberOfStarts'
         ],
          'hotwater_current_temperature': [
             'Hotwater Temperature',
@@ -170,11 +273,11 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     add_devices(sensors, True)
 
 
-class BuderusSensor(Entity):
-    """Representation of a Buderus sensor."""
+class BoschSensor(Entity):
+    """Representation of a Bosch sensor."""
 
     def __init__(self, name, bridge: BuderusBridge, sensor_type, unit, icon, km_id):
-        """Initialize the Buderus sensor."""
+        """Initialize the Bosch sensor."""
         self._name = name
         self._bridge = bridge
         self._sensor_type = sensor_type
@@ -206,10 +309,10 @@ class BuderusSensor(Entity):
     def update(self):
         """Fetch new state data for the sensor.
         This is the only method that should fetch new data for Home Assistant."""
-        _LOGGER.info("Buderus fetching data...")
+        _LOGGER.info("Bosch fetching data...")
         plain = self._bridge._get_data(self._km_id)
         if plain is not None:
             data = self._bridge._get_json(plain)
             self._state = self._bridge._get_value(data)
-        _LOGGER.info("Buderus fetching data done.")
+        _LOGGER.info("Bosch fetching data done.")
         
